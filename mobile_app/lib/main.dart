83e32f196338e5b4028299b12a7f6a7c1f308310
valeria izvoreanu <valeria.izvoreanu@gmail.com>
2022-05-19 19:41:48 +0300

    home page mobile

--- mobile_app/lib/main.dart
+++ mobile_app/lib/main.dart
@@ -14,10 +14,7 @@ class _MyStatefulWidgetState extends State<MyApp> {
   static const TextStyle optionStyle =
   TextStyle(fontSize: 30, fontWeight: FontWeight.bold);
   static const List<Widget> _widgetOptions = <Widget>[
-    Text(
-      'Index 0: Home',
-      style: optionStyle,
-    ),
+    Home(),
     Text(
       'Index 1: Business',
       style: optionStyle,
@@ -42,11 +39,15 @@ class _MyStatefulWidgetState extends State<MyApp> {
   Widget build(BuildContext context) {
     return MaterialApp(
         theme: ThemeData(
-          primarySwatch: Colors.amber,
+            primarySwatch: Colors.amber,
+            //scaffoldBackgroundColor: Colors.black,
+            brightness: Brightness.dark
         ),
         home:Scaffold(
           appBar: AppBar(
             title: const Text('Spotify'),
+            backgroundColor: Colors.amber,
+            foregroundColor: Colors.black,
           ),
           body: Center(
             child: _widgetOptions.elementAt(_selectedIndex),
@@ -79,6 +80,89 @@ class _MyStatefulWidgetState extends State<MyApp> {
             onTap: _onItemTapped,
           ),
         )
+    );
+  }
+}
+
+class Home extends StatelessWidget {
+  const Home({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return SingleChildScrollView(
+    child: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: <Widget>[
+          _buildTitle("Frequent Playlists"),
+          SizedBox(
+              height: 230,
+              child:_buildPlaylistsTiles()),
+          _buildTitle("Made For User"),
+          SizedBox(
+              height: 250,
+              child:_buildMadeForUser()),
+          _buildTitle("Other Categories"),
+          //TODO
+        ],
+      ),
+    );
+  }
+
+  Widget _buildTitle(text) {
+    return Center(child:Container(
+        padding: EdgeInsets.fromLTRB(20, 20, 20, 20),
+        child: Text(text,
+            style: TextStyle(fontSize: 22),
+            )));
+  }
+
+  Widget _buildPlaylistsTiles() {
+    return  Material(
+        child: ListView(
+          //scrollDirection: Axis.vertical,
+          children: <Widget>[
+            _playlistTile("Title", "60"),
+            _playlistTile("Title", "60"),
+            _playlistTile("Title", "60"),
+          ],
+        ),
+    );
+  }
+
+  Widget _playlistTile(name, songNr){
+    return  ListTile(
+      leading: FlutterLogo(size: 56.0),
+      title: Text(name),
+      subtitle: Text(songNr+" songs"),
+      trailing: Icon(Icons.more_vert),
+    );
+  }
+
+  Widget _buildMadeForUser() {
+    return  Material(
+      child: ListView(
+        children: <Widget>[
+          _playlistCard("name", "description"),
+          _playlistCard("name", "description"),
+          _playlistCard("name", "description"),
+        ],
+      ),
+    );
+  }
+
+  Widget _playlistCard(name, description){
+    return Card(
+        child: InkWell(
+          splashColor: Colors.blue.withAlpha(30),
+          onTap: () {
+            debugPrint('Card tapped.');
+          },
+          child: ListTile(
+            leading: Icon(Icons.album),
+            title: Text(name),
+            subtitle: Text(description),
+          )
+        ),
     );
   }
 }
